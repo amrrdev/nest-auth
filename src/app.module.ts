0d5c6242300c46658cf677221444dfbe1c02cc1d
Amr Mubarak <amrrdev@gmail.com>
2025-01-28 18:37:34 +0200

    postgres integration

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,33 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { CoffeesModule } from './coffees/coffees.module';
+import { ConfigModule, ConfigType } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { IamModule } from './iam/iam.module';
+import DatabaseConfig from './config/database.config';
 
 @Module({
-  imports: [UsersModule, CoffeesModule],
+  imports: [
+    TypeOrmModule.forRootAsync({
+      useFactory: (
+        databaseConfigrations: ConfigType<typeof DatabaseConfig>,
+      ) => ({
+        type: 'postgres',
+        host: databaseConfigrations.host,
+        database: databaseConfigrations.database,
+        port: Number(databaseConfigrations.port),
+        username: databaseConfigrations.username,
+        password: databaseConfigrations.password,
+        autoLoadEntities: true,
+        synchronize: true,
+      }),
+      inject: [DatabaseConfig.KEY],
+    }),
+    ConfigModule.forRoot({ isGlobal: true, load: [DatabaseConfig] }),
+    UsersModule,
+    CoffeesModule,
+    IamModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
