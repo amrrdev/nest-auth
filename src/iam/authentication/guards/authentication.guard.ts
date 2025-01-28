bb9f83055f477905af27464cc46ed01d4a01f895
Amr Mubarak <amrrdev@gmail.com>
2025-01-29 00:34:16 +0200

    feat(iam): implement AuthenticationGuard for flexible auth strategies
    Added AuthenticationGuard to support multiple authentication types (e.g., BEARER, NONE).
    
    Used Reflector to dynamically retrieve auth metadata from handlers and classes.
    
    Implemented a guard mapping system (authTypeGuardMap) to associate auth types with their respective guards.

---
+++ src/iam/authentication/guards/authentication.guard.ts
@@ -0,0 +1,56 @@
+import {
+  CanActivate,
+  ExecutionContext,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
+import { AUTH_TYPE_KEY } from '../decorators/auth.decorator';
+import { AuthType } from '../enum/auth-type.enum';
+import { AccessTokenGuard } from './access-token.guard';
+
+@Injectable()
+export class AuthenticationGuard implements CanActivate {
+  private static readonly defaultAuthType = AuthType.BEARER;
+  private readonly authTypeGuardMap: Record<
+    AuthType,
+    CanActivate | CanActivate[]
+  > = {
+    [AuthType.BEARER]: this.accessTokenGuard,
+    [AuthType.NONE]: { canActivate: () => true },
+  };
+
+  constructor(
+    private readonly accessTokenGuard: AccessTokenGuard,
+    private readonly reflector: Reflector,
+  ) {}
+
+  async canActivate(context: ExecutionContext): Promise<boolean> {
+    // The getAllAndOverride() method is internally
+    // designed to give method - level metadata priority over class-level metadata,
+    const authTypes = this.reflector.getAllAndOverride<AuthType[]>(
+      AUTH_TYPE_KEY,
+      [context.getHandler(), context.getClass()],
+    ) ?? [AuthenticationGuard.defaultAuthType];
+
+    const authTypesArray = Array.isArray(authTypes) ? authTypes : [authTypes];
+
+    const guards = authTypesArray
+      .map((type) => this.authTypeGuardMap[type])
+      .flat();
+    let error = new UnauthorizedException();
+
+    for (const instance of guards) {
+      const canActivate = await Promise.resolve(
+        instance.canActivate(context),
+      ).catch((err) => {
+        error = err;
+      });
+
+      if (canActivate) {
+        return true;
+      }
+    }
+    throw error;
+  }
+}
